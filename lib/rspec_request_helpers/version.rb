e0d90d34036c18f175612d836f3053ecc955bcd5
Andrii Baran <andriy.baran.v@gmail.com>
2020-01-16 12:19:48 +0200

    Bumped version

--- lib/rspec_request_helpers/version.rb
+++ lib/rspec_request_helpers/version.rb
@@ -2,8 +2,8 @@
 # @Author: Andrii Baran
 # @Date:   2019-12-18 13:05:03
 # @Last Modified by:   Andrii Baran
-# @Last Modified time: 2020-01-16 12:17:41
+# @Last Modified time: 2020-01-16 12:19:29
 
 module RspecRequestHelpers
-  VERSION = '0.3.2'
+  VERSION = '0.3.3'
 end
